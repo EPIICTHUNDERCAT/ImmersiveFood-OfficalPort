7a28a06c031eef6ff18b48a1f276635ef16f2db2
EPIICTHUNDERCAT <rvillalobos102299@gmail.com>
2017-09-08 02:58:34 -0700

    Crash Updates

--- 1.12.1/src/main/java/com/hedgehogpie12/food/mod/Reference.java
+++ 1.12.1/src/main/java/com/hedgehogpie12/food/mod/Reference.java
@@ -3,7 +3,7 @@ package com.hedgehogpie12.food.mod;
 public class Reference {
 	public static final String MODID = "immersivefood";
 	public static final String MOD_NAME = "Immersive Food";
-	public static final String VERSION = "1.12.1-0.0.1";
+	public static final String VERSION = "1.12.1-0.0.2";
 	public static final String ACCEPTEDVERSION = "[1.12,1.13)";
 
 	public static final String CLIENT_PROXY = "com.hedgehogpie12.food.mod.proxy.ClientProxy";
